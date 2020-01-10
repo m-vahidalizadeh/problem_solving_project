8fec86c65019511ef1d13099a6d4fb96dc8029eb
mohammad <m_vahidalizadeh@live.com>
2020-01-09 21:41:50 -0500

    Commit some new solutions.

---
+++ src/MSKnightDialler.java
@@ -0,0 +1,11 @@
+public class MSKnightDialler {
+
+    public static void main(String[] args){
+        System.out.println(knightDialer(1));
+    }
+
+    public static int knightDialer(int N) {
+
+    }
+
+}
