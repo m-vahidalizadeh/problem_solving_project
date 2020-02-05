436b21b1a47aa05b1b19e55c25b75ee0c8913100
mohammad <m_vahidalizadeh@live.com>
2020-02-05 08:57:43 -0500

    Commit some new solutions.

---
+++ src/MSFindingAnagrams.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class MSFindingAnagrams {
+
+    public static void main(String[] args) {
+/*
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+ */
+        System.out.println(isAnagram("anagram", "nagaram"));
+        System.out.println(isAnagram("rat", "car"));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        Arrays.sort(sChars);
+        char[] tChars = t.toCharArray();
+        Arrays.sort(tChars);
+        String sortedS = new String(sChars);
+        String sortedT = new String(tChars);
+        return sortedS.equals(sortedT);
+    }
+
+}
