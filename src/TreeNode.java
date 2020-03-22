83b2c14690d236c600a657bae821fc94349c0ade
mohammad <m_vahidalizadeh@live.com>
2020-03-21 22:17:44 -0400

    Commit some new solutions.

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,11 +1,19 @@
 public class TreeNode {
 
-    int val;
+    Integer val;
     TreeNode left;
     TreeNode right;
 
-    TreeNode(int x) {
+    TreeNode(Integer x) {
         val = x;
+    }
+
+    TreeNode(TreeNode treeNode) {
+        if (treeNode != null) {
+            this.val = treeNode.val;
+            this.left = new TreeNode(treeNode.left);
+            this.right = new TreeNode(treeNode.right);
+        }
     }
 
 }
