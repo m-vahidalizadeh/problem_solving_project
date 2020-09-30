7982cf3d0b5cfd1b213eb02a895324985403258e
mohammad <m_vahidalizadeh@live.com>
2020-09-29 23:35:54 -0400

    Adding some solutions.

--- src/leetcode/companies/google/Decompress.java
+++ src/leetcode/companies/google/Decompress.java
@@ -2,6 +2,33 @@ package leetcode.companies.google;
 
 import java.util.Stack;
 
+/**
+ * Your input is a compressed string of the format number[string] and the decompressed output form should be the string written number times. For example:
+ * <p>
+ * The input
+ * <p>
+ * 3[abc]4[ab]c
+ * <p>
+ * Would be output as
+ * <p>
+ * abcabcabcababababc
+ * <p>
+ * Other rules
+ * <p>
+ * Number can have more than one digit. For example, 10[a] is allowed, and just means aaaaaaaaaa
+ * <p>
+ * One repetition can occur inside another. For example, 2[3[a]b] decompresses into aaabaaab
+ * <p>
+ * Characters allowed as input include digits, small English letters and brackets [ ].
+ * <p>
+ * Digits are only to represent amount of repetitions.
+ * <p>
+ * Letters are just letters.
+ * <p>
+ * Brackets are only part of syntax of writing repeated substring.
+ * <p>
+ * Input is always valid, so no need to check its validity.
+ */
 public class Decompress {
 
     public String decompress(String compressed) {
