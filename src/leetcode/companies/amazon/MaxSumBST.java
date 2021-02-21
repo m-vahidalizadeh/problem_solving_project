6889565b045bb3d48fe61c7f9ef57c0d4568305c
mohammad <m_vahidalizadeh@live.com>
2021-02-21 11:59:42 -0500

    Adding some solutions.

---
+++ src/leetcode/companies/amazon/MaxSumBST.java
@@ -0,0 +1,99 @@
+package leetcode.companies.amazon;
+
+/**
+ * 1373. Maximum Sum BST in Binary Tree
+ * Given a binary tree root, the task is to return the maximum sum of all keys of any sub-tree which is also a Binary Search Tree (BST).
+ *
+ * Assume a BST is defined as follows:
+ *
+ * The left subtree of a node contains only nodes with keys less than the node's key.
+ * The right subtree of a node contains only nodes with keys greater than the node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ *
+ * Example 1:
+ *
+ * Input: root = [1,4,3,2,4,2,5,null,null,null,null,null,null,4,6]
+ * Output: 20
+ * Explanation: Maximum sum in a valid Binary search tree is obtained in root node with key equal to 3.
+ * Example 2:
+ *
+ * Input: root = [4,3,null,1,2]
+ * Output: 2
+ * Explanation: Maximum sum in a valid Binary search tree is obtained in a single root node with key equal to 2.
+ * Example 3:
+ *
+ * Input: root = [-4,-2,-5]
+ * Output: 0
+ * Explanation: All values are negatives. Return an empty BST.
+ * Example 4:
+ *
+ * Input: root = [2,1,3]
+ * Output: 6
+ * Example 5:
+ *
+ * Input: root = [5,4,8,3,null,6,3]
+ * Output: 7
+ *
+ * Constraints:
+ *
+ * The given binary tree will have between 1 and 40000 nodes.
+ * Each node's value is between [-4 * 10^4 , 4 * 10^4].
+ */
+public class MaxSumBST {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Result {
+        int sum;
+        int min;
+        int max;
+        boolean isBst;
+
+        public Result(boolean isBst) {
+            this.sum = 0;
+            this.min = Integer.MAX_VALUE;
+            this.max = Integer.MIN_VALUE;
+            this.isBst = isBst;
+        }
+    }
+
+    int maxSum;
+
+    public int maxSumBST(TreeNode root) {
+        findMax(root);
+        return maxSum;
+    }
+
+    private Result findMax(TreeNode node) {
+        if (node == null) return new Result(true);
+        Result lR = findMax(node.left);
+        Result rR = findMax(node.right);
+        Result curr = new Result(false);
+        curr.min = Math.min(node.val, Math.min(lR.min, rR.min));
+        curr.max = Math.max(node.val, Math.max(lR.max, rR.max));
+        curr.isBst = lR.isBst && rR.isBst && lR.max < node.val && node.val < rR.min;
+        if (curr.isBst) {
+            curr.sum = lR.sum + rR.sum + node.val;
+            maxSum = Math.max(maxSum, curr.sum);
+        }
+        return curr;
+    }
+
+}
