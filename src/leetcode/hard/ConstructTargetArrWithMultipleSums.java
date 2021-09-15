719815afb27012cc789fa911a58c129ba7ca6315
mohammad <m_vahidalizadeh@live.com>
2021-09-15 09:09:49 -0400

    Adding solution of construct target arrays with multiple sums.

---
+++ src/leetcode/hard/ConstructTargetArrWithMultipleSums.java
@@ -0,0 +1,61 @@
+package leetcode.hard;
+
+import java.util.PriorityQueue;
+
+/**
+ * 1354. Construct Target Array With Multiple Sums
+ * You are given an array target of n integers. From a starting array arr consisting of n 1's, you may perform the following procedure :
+ *
+ * let x be the sum of all elements currently in your array.
+ * choose index i, such that 0 <= i < n and set the value of arr at index i to x.
+ * You may repeat this procedure as many times as needed.
+ * Return true if it is possible to construct the target array from arr, otherwise, return false.
+ *
+ * Example 1:
+ *
+ * Input: target = [9,3,5]
+ * Output: true
+ * Explanation: Start with arr = [1, 1, 1]
+ * [1, 1, 1], sum = 3 choose index 1
+ * [1, 3, 1], sum = 5 choose index 2
+ * [1, 3, 5], sum = 9 choose index 0
+ * [9, 3, 5] Done
+ * Example 2:
+ *
+ * Input: target = [1,1,1,2]
+ * Output: false
+ * Explanation: Impossible to create target array from [1,1,1,1].
+ * Example 3:
+ *
+ * Input: target = [8,5]
+ * Output: true
+ *
+ * Constraints:
+ *
+ * n == target.length
+ * 1 <= n <= 5 * 104
+ * 1 <= target[i] <= 109
+ */
+public class ConstructTargetArrWithMultipleSums {
+
+    public boolean isPossible(int[] target) {
+        if (target.length == 1) return target[0] == 1;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        int total = 0;
+        for (int num : target) {
+            maxHeap.add(num);
+            total += num;
+        }
+        while (maxHeap.peek() > 1) {
+            int max = maxHeap.poll();
+            int remaining = total - max;
+            if (remaining == 1) return true; // ex. (1,1000)->(1,999)->...(1,1)
+            int pre = max % remaining;
+            if (pre < 1 || pre == max) return false;
+            maxHeap.add(pre);
+            total = total + pre - max;
+        }
+        return true;
+    }
+
+}
