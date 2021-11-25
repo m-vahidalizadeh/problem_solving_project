1810351ab8398a8e4f0f940d3e637b82e96a23aa
mohammad <m_vahidalizadeh@live.com>
2021-11-25 11:47:35 -0500

    Adding some solutions.

---
+++ src/leetcode/hard/RussianDollEnvelopes.java
@@ -0,0 +1,52 @@
+package leetcode.hard;
+
+import java.util.Arrays;
+
+/**
+ * 354. Russian Doll Envelopes
+ * You are given a 2D array of integers envelopes where envelopes[i] = [wi, hi] represents the width and the height of an envelope.
+ *
+ * One envelope can fit into another if and only if both the width and height of one envelope are greater than the other envelope's width and height.
+ *
+ * Return the maximum number of envelopes you can Russian doll (i.e., put one inside the other).
+ *
+ * Note: You cannot rotate an envelope.
+ *
+ * Example 1:
+ *
+ * Input: envelopes = [[5,4],[6,4],[6,7],[2,3]]
+ * Output: 3
+ * Explanation: The maximum number of envelopes you can Russian doll is 3 ([2,3] => [5,4] => [6,7]).
+ * Example 2:
+ *
+ * Input: envelopes = [[1,1],[1,1],[1,1]]
+ * Output: 1
+ *
+ * Constraints:
+ *
+ * 1 <= envelopes.length <= 5000
+ * envelopes[i].length == 2
+ * 1 <= wi, hi <= 104
+ */
+public class RussianDollEnvelopes {
+
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]); // Increasing on width, decreasing on height
+        int[] secondDim = new int[envelopes.length];
+        for (int i = 0; i < envelopes.length; i++) secondDim[i] = envelopes[i][1];
+        return lengthOfLIS(secondDim);
+    }
+
+    private int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
+        for (int num : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, num);
+            if (i < 0) i = -(i + 1);
+            dp[i] = num;
+            if (i == len) len++;
+        }
+        return len;
+    }
+
+}
