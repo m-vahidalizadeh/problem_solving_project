5ed2bb5820014f7a1c22b9605707a419f7c118ac
mohammad <m_vahidalizadeh@live.com>
2021-12-27 15:23:36 -0500

    Adding solution of kth smallest lexicographical order.

---
+++ src/leetcode/hard/KthSmallestInLexicographicalOrder.java
@@ -0,0 +1,48 @@
+package leetcode.hard;
+
+/**
+ * 440. K-th Smallest in Lexicographical Order
+ * Given two integers n and k, return the kth lexicographically smallest integer in the range [1, n].
+ *
+ * Example 1:
+ *
+ * Input: n = 13, k = 2
+ * Output: 10
+ * Explanation: The lexicographical order is [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9], so the second smallest number is 10.
+ * Example 2:
+ *
+ * Input: n = 1, k = 1
+ * Output: 1
+ *
+ * Constraints:
+ *
+ * 1 <= k <= n <= 109
+ */
+public class KthSmallestInLexicographicalOrder {
+
+    public int findKthNumber(int n, int k) {
+        long curr = 1;
+        while (k > 1) {
+            long gap = findGap(curr, curr + 1, n);
+            if (gap <= k - 1) {
+                k -= gap;
+                curr++;
+            } else {
+                k--;
+                curr *= 10;
+            }
+        }
+        return (int) curr;
+    }
+
+    private long findGap(long a, long b, int n) {
+        long gap = 0;
+        while (a <= n) {
+            gap += Math.min(n + 1, b) - a;
+            a *= 10;
+            b *= 10;
+        }
+        return gap;
+    }
+
+}
