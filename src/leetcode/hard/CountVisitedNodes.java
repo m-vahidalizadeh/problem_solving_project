db273430ed8b0211fa17cb387801cf3a55053fd9
mohammad <m_vahidalizadeh@live.com>
2024-05-28 08:40:27 -0400

    Adding some solutions.

---
+++ src/leetcode/hard/CountVisitedNodes.java
@@ -0,0 +1,72 @@
+package leetcode.hard;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+
+/**
+ * 2876. Count Visited Nodes in a Directed Graph
+ * Hard
+ *
+ * Topics
+ *
+ * Companies
+ *
+ * Hint
+ * There is a directed graph consisting of n nodes numbered from 0 to n - 1 and n directed edges.
+ *
+ * You are given a 0-indexed array edges where edges[i] indicates that there is an edge from node i to node edges[i].
+ *
+ * Consider the following process on the graph:
+ *
+ * You start from a node x and keep visiting other nodes through edges until you reach a node that you have already visited before on this same process.
+ * Return an array answer where answer[i] is the number of different nodes that you will visit if you perform the process starting from node i.
+ *
+ * Example 1:
+ *
+ * Input: edges = [1,2,0,0]
+ * Output: [3,3,3,4]
+ * Explanation: We perform the process starting from each node in the following way:
+ * - Starting from node 0, we visit the nodes 0 -> 1 -> 2 -> 0. The number of different nodes we visit is 3.
+ * - Starting from node 1, we visit the nodes 1 -> 2 -> 0 -> 1. The number of different nodes we visit is 3.
+ * - Starting from node 2, we visit the nodes 2 -> 0 -> 1 -> 2. The number of different nodes we visit is 3.
+ * - Starting from node 3, we visit the nodes 3 -> 0 -> 1 -> 2 -> 0. The number of different nodes we visit is 4.
+ * Example 2:
+ *
+ * Input: edges = [1,2,3,4,0]
+ * Output: [5,5,5,5,5]
+ * Explanation: Starting from any node we can visit every node in the graph in the process.
+ *
+ * Constraints:
+ *
+ * n == edges.length
+ * 2 <= n <= 10^5
+ * 0 <= edges[i] <= n - 1
+ * edges[i] != i
+ */
+public class CountVisitedNodes {
+
+    public int[] countVisitedNodes(List<Integer> edges) {
+        int n = edges.size(), next = 0, res[] = new int[n];
+        for (int start = 0; start < n; next = ++start) {
+            Set<Integer> seen = new HashSet<>();
+            Stack<Integer> s = new Stack<>();
+            while (!seen.contains(next) && res[next] == 0) {
+                seen.add(next);
+                s.push(next);
+                next = edges.get(next);
+            }
+            if (seen.contains(next)) {
+                int k = s.size() - s.indexOf(next);
+                for (int a = 0; a < k; a++) res[s.pop()] = k;
+            }
+            while (!s.isEmpty()) {
+                int curr = s.pop();
+                res[curr] = res[edges.get(curr)] + 1;
+            }
+        }
+        return res;
+    }
+
+}
